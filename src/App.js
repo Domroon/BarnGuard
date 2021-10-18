f5a0dc79be3fb16b2741e8a3a3192d706ac29c85
Dominik Haeusser <haeusser-dominik@web.de>
2021-10-18 11:10:39 +0200

    add redirect after register

--- src/App.js
+++ src/App.js
@@ -14,10 +14,11 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false)
   const [videoname, setVideoname] = useState(null)
   const [access_token, setAccessToken] = useState(localStorage.getItem("access_token"))
+  const [register_access, setRegister_access] = useState()
 
   return (
     <>
-      <StateContext.Provider value={{ setLoggedIn, setVideoname, videoname, access_token, setAccessToken }}>
+      <StateContext.Provider value={{ setLoggedIn, setVideoname, videoname, access_token, setAccessToken, setRegister_access, register_access }}>
         <BrowserRouter>
           {loggedIn ? <Header /> : <ReducedHeader />}
           <Switch>
@@ -27,9 +28,7 @@ function App() {
             <Route exact path="/">
               {loggedIn ? <Redirect to="/videos" /> : <Login />}
             </Route>
-            <Route path="/registrieren">
-              <Register />
-            </Route>
+            <Route path="/registrieren">{register_access ? <Redirect to="/" /> : <Register />}</Route>
             <Route path="/singleVideo/:videoname" component={SingleVideo} />
           </Switch>
           <Footer />
