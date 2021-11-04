a4cf2269e7c1aebf585afdf0d1bf2a23aef0193b
Dominik Haeusser <haeusser-dominik@web.de>
2021-11-04 08:58:37 +0100

    delete no longer needed functions

--- test_software/upload_test.py
+++ test_software/upload_test.py
@@ -16,97 +16,6 @@ BASE_PATH = Path(getcwd())
 FILES_UPLOAD = BASE_PATH / "files_upload"
 
 
-def upload_video(filename):
-    try:
-        url = f'http://{NETWORK_ADDRESS}/upload'
-        video_file = open(str(FILES_UPLOAD/filename), 'rb')
-        files = {'file' : video_file}
-
-        r = requests.post(url, files=files, headers={'Authorization' : 'gAAAAABhMhDkkS0ZWFKyrhFBnDxJp5r_cjV-ZXFYh4adcoCMRSwo_qcnfsqadt4nwD3XXBlYXNHNBJWyEB7FeH6qR_FVnxFa-NGLI2HPGBYCnY2avAdd5UJ1fBOR5YoVVR5O7iXE9rpnZKRWdkUAsyQ5zuQA_XquSukJvwziExE6a5TW4NTw3xQ='})
-
-        video_file.close()
-
-        if r.status_code == 200:
-            os.remove(str(FILES_UPLOAD/filename))
-    except Exception as error:
-        print(f'Upload Video Error: \n{error}')
-        raise error
-
-    return r
-
-
-def upload(videoname, raw_video_name):
-    video_response = upload_video(videoname)
-    print(f'VIDEO RESPONSE: \nRESPONSE CODE: {video_response.status_code}\nRESPONSE BODY: "{video_response.text}"')
-    
-    # in production datetime.now() !!!
-    #rand_date, rand_time = gen_random_datetime()
-    date = str(DateTime.now()).split()[0]
-    long_time = str(DateTime.now()).split()[1]
-    time=long_time.split('.')[0]
-    json_video_response = upload_video_json(date, time, raw_video_name)
-    print(f'VIDEO_JSON RESPONSE: \nRESPONSE CODE: {json_video_response.status_code}\nRESPONSE BODY: "{json_video_response.text}"')
-
-    if video_response.status_code != 200 and json_video_response.status_code != 201:
-        return True
-    else:
-        return False
-
-
-def upload_video_json(date, time, raw_video_name):
-    try:
-        url = f'http://{NETWORK_ADDRESS}/api/videos'
-        payload = json.dumps(generate_video_json(date, time, raw_video_name))
-        return requests.post(url, data=payload, headers={'Content-Type': 'application/json', 'Authorization' : 'gAAAAABhMhDkkS0ZWFKyrhFBnDxJp5r_cjV-ZXFYh4adcoCMRSwo_qcnfsqadt4nwD3XXBlYXNHNBJWyEB7FeH6qR_FVnxFa-NGLI2HPGBYCnY2avAdd5UJ1fBOR5YoVVR5O7iXE9rpnZKRWdkUAsyQ5zuQA_XquSukJvwziExE6a5TW4NTw3xQ='})
-    except Exception as error:
-        logger.error(f'Upload Video Json Error: \n{error}')
-        raise error
-
-    # print(f'Post Json-Data for {raw_video_name}.mp4')
-    # print(f'POST: {payload}')
-    # print(f'Response Code {r.status_code}')
-    # print(f'Response Body:')
-    # print(r.text)
-
-
-def generate_json_file(date, time, filename):
-    filename, file_type = filename.split('.')
-    if file_type == 'mp4':
-        json_data = {
-            "date": date,
-            "thumbnail_photo" : f'{filename}.jpg',
-            "time" : time,
-            "videoname" : f'{filename}.mp4',
-        }
-        return json_data
-    else:
-        return 'wrong file type'
-
-
-def generate_formatted_timestamp():
-    now = str(DateTime.now()).split(' ')
-
-    # Date
-    date_now = now[0]
-    formatted_date = format_date(date_now)
-
-    # Time
-    time_now = now[1]    
-    formatted_time = format_time(time_now)
-
-    return formatted_date, formatted_time
-
-
-def format_date(date_now):
-    date_items = date_now.split('-')
-    return f'{date_items[0]}-{date_items[1]}-{date_items[2]}'
-
-
-def format_time(time_now):
-    time_items = time_now.split(':')
-    return f"{time_items[0]}:{time_items[1]}"
-
-
 class FileTransmitter:
     def __init__(self, logger):
         self.logger = logger
@@ -210,9 +119,6 @@ def main():
     transmit_logger.setLevel(logging.DEBUG)
     transmit_logger.addHandler(console_handler)
     transmit_logger.addHandler(file_handler)
-
-    # upload_video('ruehrwerk.jpg')
-    # print(listdir('files_upload'))
 
     transmitter = FileTransmitter(transmit_logger)
     transmitter.start()
